8957e4e6685996dc2486a134a209bf6a7f8dde96
Applepie9 <horel.dora@gmail.com>
2024-07-30 08:18:25 +0100

    Theory tests with if-else solution

---
+++ FizzBuzz/FizzBuzzGame.cs
@@ -0,0 +1,16 @@
+﻿namespace FizzBuzz
+{
+    public static class FizzBuzzGame
+    {
+        public static string Play(int number)
+        {
+            if(number % 15 == 0)
+                return "FizzBuzz";
+            if(number % 5 == 0)
+                return "Buzz";
+            if(number % 3 == 0)
+                return "Fizz";
+            return number.ToString();
+        }
+    }
+}
